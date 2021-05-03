2348a8a17b16ec71880ae61c256f0fd30726aff9
Karlo <karlo.schubert@datenkraft.com>
2021-05-03 16:03:35 +0200

    BB-7 use client to create itself with factory

--- src/Client.php
+++ src/Client.php
@@ -1,8 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Datenkraft\Backbone\Client\SkuUsageApi;
 
+use Datenkraft\Backbone\Client\BaseApi\ClientFactory;
+
+/**
+ * Class Client
+ * @package Datenkraft\Backbone\Client\SkuUsageApi
+ */
 class Client extends \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Client
 {
+    /**
+     * @param ClientFactory $clientFactory
+     * @param string|null $endpointUrl
+     * @return static
+     */
+    public static function createWithFactory(ClientFactory $clientFactory, string $endpointUrl = null): self
+    {
+        $endpointUrl = $endpointUrl ?? getenv('X_DATENKRAFT_SKU_USAGE_URL') ?: null;
 
+        return $clientFactory->createClient(static::class, $endpointUrl);
+    }
 }
