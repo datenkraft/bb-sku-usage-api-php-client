6e46c59f21d15de582e412a099c4661293f1da27
sandra.kocic <sandra.kocic@datenkraft.com>
2021-09-13 15:46:30 +0200

    update client

---
+++ src/Generated/Exception/GetAuthRoleCollectionEndpointInternalServerErrorException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\SkuUsageApi\Generated\Exception;
+
+class GetAuthRoleCollectionEndpointInternalServerErrorException extends InternalServerErrorException
+{
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Server error', 500);
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse()
+    {
+        return $this->errorResponse;
+    }
+}
