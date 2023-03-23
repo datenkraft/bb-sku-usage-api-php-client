8e784991bedf251eebe9ae63a930719437a32197
CircleCI <office@datenkraft.com>
2023-03-23 09:56:27 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/PatchAuthRoleInternalServerErrorException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\SkuUsageApi\Generated\Exception;
+
+class PatchAuthRoleInternalServerErrorException extends InternalServerErrorException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Server error');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
