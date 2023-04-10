2f31267fbe29dae083479da79948af10da6b8757
CircleCI <office@datenkraft.com>
2023-04-10 15:18:25 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/DeleteAuthRoleBadRequestException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\SkuUsageApi\Generated\Exception;
+
+class DeleteAuthRoleBadRequestException extends BadRequestException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Bad Request');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
