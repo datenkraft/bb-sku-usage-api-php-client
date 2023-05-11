e7e33745023e10074629a6fa41e6aa666e73032b
CircleCI <office@datenkraft.com>
2023-05-11 14:44:31 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/DeleteAuthPermissionRoleCollectionNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\SkuUsageApi\Generated\Exception;
+
+class DeleteAuthPermissionRoleCollectionNotFoundException extends NotFoundException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Not Found');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
