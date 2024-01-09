d114ff651851b23c878734f6910ae616fa7fd271
Datenkraft <office@datenkraft.com>
2024-01-09 07:26:55 +0000

    Autogenerated client update [publish]

--- src/Generated/Exception/GetTransactionForbiddenException.php
+++ src/Generated/Exception/GetTransactionForbiddenException.php
@@ -14,7 +14,10 @@ class GetTransactionForbiddenException extends ForbiddenException
     private $response;
     public function __construct(\Datenkraft\Backbone\Client\SkuUsageApi\Generated\Model\ErrorResponse $errorResponse, \Psr\Http\Message\ResponseInterface $response)
     {
-        parent::__construct('Forbidden');
+        parent::__construct('Forbidden
+
+Error codes:
+- PERMISSIONS_MISSING: No authorization for the called action was found.');
         $this->errorResponse = $errorResponse;
         $this->response = $response;
     }
